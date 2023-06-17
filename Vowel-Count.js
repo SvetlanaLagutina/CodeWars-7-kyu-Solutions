9ba4a6e92c8f276c0752c06275b1ff0140b0bbbf
Lagutina Svetlana <lagutinasvetlana@hotmail.com>
2023-06-17 10:28:10 +0300

    added new codewars challenge

---
+++ Vowel-Count.js
@@ -0,0 +1,16 @@
+// Vowel Count
+
+// DESCRIPTION:
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+// -STRINGS,
+// -FUNDAMENTALS
+
+function getCount(str) {
+    let count = str.match(/[aeiou]/gi);
+    return count === null ? 0 : count.length;
+  }
