b7728764441c0ccbbc3df87fd7d3268411350679
Lagutina Svetlana <lagutinasvetlana@hotmail.com>
2023-06-17 11:28:32 +0300

    new file

---
+++ Shortest-Word.js
@@ -0,0 +1,12 @@
+// Shortest Word
+
+// DESCRIPTION:
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+// -FUNDAMENTALS
+
+function findShort(s){
+    return s.split(" ").map(item => item.length).sort(function(a, b) { return a - b; })[0];
+  }
